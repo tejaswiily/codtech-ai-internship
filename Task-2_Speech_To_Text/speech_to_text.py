1b84d20ebe24bedc40b30aa147bae4d1c0838cf1
Tejaswi Venkata Sai Nallamilli <141433657+tejaswiily@users.noreply.github.com>
2026-02-07 18:27:57 +0530

    Update speech_to_text.py

--- Task-2_Speech_To_Text/speech_to_text.py
+++ Task-2_Speech_To_Text/speech_to_text.py
@@ -1,5 +1,6 @@
-import speech to text as sr
-def speech_to_text (audio_file):
+import speech_recognition as sr
+
+def speech_to_text(audio_file):
     recognizer = sr.Recognizer()
     with sr.AudioFile(audio_file) as source:
         audio = recognizer.record(source)
@@ -8,12 +9,16 @@ def speech_to_text (audio_file):
             return text
         except Exception as e:
             return f"Error: {str(e)}"
+
 def main():
     print("=== SPEECH TO TEXT TOOL ===")
     audio_file = input("Enter the path of your audio file (.wav): ")
+
     print("\nTranscribing...")
     result = speech_to_text(audio_file)
+
     print("\n=== TRANSCRIPTION ===")
     print(result)
+
 if __name__ == "__main__":
     main()
