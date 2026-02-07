3b02a7fe3049e777f053b0015d3820b9207c160e
Tejaswi Venkata Sai Nallamilli <141433657+tejaswiily@users.noreply.github.com>
2026-02-07 16:28:51 +0530

    Add files via upload

---
+++ Task-2_Speech_To_Text/speech_to_text.py
@@ -0,0 +1,19 @@
+import speech to text as sr
+def speech_to_text (audio_file):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio = recognizer.record(source)
+        try:
+            text = recognizer.recognize_google(audio)
+            return text
+        except Exception as e:
+            return f"Error: {str(e)}"
+def main():
+    print("=== SPEECH TO TEXT TOOL ===")
+    audio_file = input("Enter the path of your audio file (.wav): ")
+    print("\nTranscribing...")
+    result = speech_to_text(audio_file)
+    print("\n=== TRANSCRIPTION ===")
+    print(result)
+if __name__ == "__main__":
+    main()
